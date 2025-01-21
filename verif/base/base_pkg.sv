bdcef1b6da64f6cbf1efeaffc3600acdd779dac4
George Castillo <4260836+gmcastil@users.noreply.github.com>
2025-01-21 07:26:43 -0700

    I think i have a minimal example that I can start using to implement the classes now.

---
+++ verif/base/base_pkg.sv
@@ -0,0 +1,13 @@
+package base_pkg;
+
+    import common_pkg::*;
+
+`include "component_base.sv"
+`include "env_base.sv"
+`include "monitor_base.sv"
+`include "scoreboard_base.sv"
+`include "sequencer_base.sv"
+`include "driver_base.sv"
+
+endpackage: base_pkg
+
---
+++ verif/tests/tests_pkg.sv
@@ -0,0 +1,10 @@
+package tests_pkg;
+
+    import common_pkg::*;
+    import base_pkg::*;
+
+`include "base_test.sv"
+`include "env_test.sv"
+
+endpackage: tests_pkg
+
