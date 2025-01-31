c2d69782ad8152587fd997377506f6d4bdbe5e5a
George Castillo <4260836+gmcastil@users.noreply.github.com>
2025-01-31 04:58:09 -0700

    Better make integration
    - I removed the `lib` target requirement so that it can be called in a cleaner
    way from a primary makefile
    - Dig this way. So much more straightforward than what I had been doing

--- verif/verif.mk
+++ verif/verif.mk
@@ -4,11 +4,11 @@
 VERIF_ROOT		:= $(dir $(lastword $(MAKEFILE_LIST)))
 
 .PHONY: verif
-verif: lib
+verif:
 	@if [[ -z "$(WORK)" ]]; then \
 		printf '%s\n' "Error: WORK is unset" >&2; \
 		exit 1; \
 	fi
-	$(VLOG) -work $(WORK) +INCDIR+$(VERIF_ROOT)/common -sv $(VERIF_ROOT)/common/common_pkg.sv
-	$(VLOG) -work $(WORK) +INCDIR+$(VERIF_ROOT)/base -sv $(VERIF_ROOT)/base/base_pkg.sv
+	$(VLOG) -work $(WORK) +INCDIR+$(VERIF_ROOT)/common $(VLOG_FLAGS) $(VERIF_ROOT)/common/common_pkg.sv
+	$(VLOG) -work $(WORK) +INCDIR+$(VERIF_ROOT)/base $(VLOG_FLAGS) $(VERIF_ROOT)/base/base_pkg.sv
 
