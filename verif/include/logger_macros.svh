a6ef37fb01e82fda82a5c3fb85d96cda50172e3c
George Castillo <4260836+gmcastil@users.noreply.github.com>
2025-02-23 05:38:26 -0700

    Thanks heavens for the `-E` flag

--- verif/include/logger_macros.svh
+++ verif/include/logger_macros.svh
@@ -2,7 +2,7 @@
 `define LOGGER_MACROS_SVH
 
 `define log_fatal(msg) \
-    logger::get_instance().log(LOG_FATAL, LOG_NONE, msg, this, `__FILE__, `__LINE__) \
+    logger::get_instance().log(LOG_FATAL, LOG_NONE, msg, this, `__FILE__, `__LINE__); \
 `ifndef NO_STACKTRACE_SUPPORT \
     $stacktrace; \
 `endif \
@@ -10,7 +10,7 @@
     $fatal(1)
 
 `define log_fatal_with_context(msg, cntxt) \
-    logger::get_instance().log(LOG_FATAL, LOG_NONE, msg, cntxt, `__FILE__, `__LINE__) \
+    logger::get_instance().log(LOG_FATAL, LOG_NONE, msg, cntxt, `__FILE__, `__LINE__); \
 `ifndef NO_STACKTRACE_SUPPORT \
     $stacktrace; \
 `endif \
@@ -35,11 +35,11 @@
 `define log_info_with_context(cntxt, msg, verbosity) \
     if (cntxt.get_log_level() >= verbosity) logger::get_instance().log(LOG_INFO, verbosity, msg, cntxt, `__FILE__, `__LINE__)
 
-`define log_debug(cntxt, msg) \
-    if (this.get_log_level() >= LOG_DEBUG) logger::get_instance().log(LOG_INFO, LOG_DEBUG, msg, this, `__FILE__, `__LINE__)
-
 `define log_debug(msg) \
     if (this.get_log_level() >= LOG_DEBUG) logger::get_instance().log(LOG_INFO, LOG_DEBUG, msg, cntxt, `__FILE__, `__LINE__)
+
+`define log_debug_with_context(cntxt, msg) \
+    if (this.get_log_level() >= LOG_DEBUG) logger::get_instance().log(LOG_INFO, LOG_DEBUG, msg, this, `__FILE__, `__LINE__)
 
 `endif
 
